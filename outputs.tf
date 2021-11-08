73994e3b785a108aa02658ebd6926cba0b7f10cf
Attila Patel <attila_patel@hotmail.com>
2021-11-08 16:20:04 +0000

    updated output

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "aviatrix_controller_private_ip" {
 output "aviatrix_copilot_public_ip" {
   value =  azurerm_public_ip.avx-copilot-public-ip.ip_address
 }
-
+/*
 output "fortinet_fortimanager_public_ip" {
   value =  azurerm_public_ip.fnt-manager-public-ip.ip_address
 }
@@ -19,3 +19,4 @@ output "fortinet_fortimanager_public_ip" {
 output "fortinet_fortimanager_private_ip" {
   value =  azurerm_network_interface.fnt-manager-iface.private_ip_address
 }
+*/
