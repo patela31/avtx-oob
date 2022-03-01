c4b4c4b02d2e07b4e5f4ff821d3683144d929d86
patelavtx <apatel@aviatrix.com>
2022-03-01 14:40:30 +0000

    output.tf

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "aviatrix_controller_public_ip" {
 output "aviatrix_controller_private_ip" {
   value = azurerm_network_interface.avx-ctrl-iface.private_ip_address
 }
-
+/*
 output "copilot_public_ip" {
   value = module.copilot_build_azure.public_ip
 }
@@ -15,9 +15,7 @@ output "copilot_public_ip" {
 output "copilot_private_ip" {
   value = module.copilot_build_azure.private_ip
 }
-
-/*
+*/
 output "aviatrix_copilot_public_ip" {
   value =  azurerm_public_ip.avx-copilot-public-ip.ip_address
 }
-*/
